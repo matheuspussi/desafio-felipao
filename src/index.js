465db4f083117b1406e71f0f5010e2c1cd0c5113
matheuspussi <64718054+matheuspussi@users.noreply.github.com>
2025-06-30 04:06:28 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,37 +4,36 @@ let nivelHeroi = ""
 
 switch (true) {
 	case xPHeroi <= 1000:
-    nivelHeroi = "Ferro";
-    break
+    	nivelHeroi = "Ferro";
+   	break
     
-    case xPHeroi >= 1001 && xPHeroi <= 2000:
-    nivelHeroi = "Bronze";
-    break
+    	case xPHeroi >= 1001 && xPHeroi <= 2000:
+    	nivelHeroi = "Bronze";
+    	break
     
-    case xPHeroi >= 2001 && xPHeroi <= 5000:
-    nivelHeroi = "Prata";
-    break
+    	case xPHeroi >= 2001 && xPHeroi <= 5000:
+    	nivelHeroi = "Prata";
+    	break
     
-    case xPHeroi >= 5001 && xPHeroi <= 7000:
-    nivelHeroi = "Ouro";
-    break
+    	case xPHeroi >= 5001 && xPHeroi <= 7000:
+    	nivelHeroi = "Ouro";
+    	break
     
-    case xPHeroi >= 7001 && xPHeroi <= 8000:
-    nivelHeroi = "Platina";
-    break
+    	case xPHeroi >= 7001 && xPHeroi <= 8000:
+    	nivelHeroi = "Platina";
+    	break
     
-    case xPHeroi >= 8001 && xPHeroi <= 9000:
-    nivelHeroi = "Ascendente";
-    break
+    	case xPHeroi >= 8001 && xPHeroi <= 9000:
+   	nivelHeroi = "Ascendente";
+    	break
     
-    case xPHeroi >= 9001 && xPHeroi <= 10000:
-    nivelHeroi = "Imortal";
-    break
+    	case xPHeroi >= 9001 && xPHeroi <= 10000:
+    	nivelHeroi = "Imortal";
+    	break
     
-    case xPHeroi >= 10001:
-    nivelHeroi = "Radiante";
-    break
-     
+    	case xPHeroi >= 10001:
+    	nivelHeroi = "Radiante";
+    	break
 }
 
 console.log("O Herói de nome " + nomeHeroi + " está no nível " + nivelHeroi)
